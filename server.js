4c20e750a7d4be0ad5da097c23bfed936b196e9c
aninaslyan <ani.naslyan@gmail.com>
2021-07-08 13:06:54 +0400

    auth server with programmatic way

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
-const jsonServer = require('json-server');
+const jsonServer = require('json-server')
+const auth = require('json-server-auth')
+
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
-server.use(middlewares);
+server.db = router.db;
+
+// Permission rules
+const rules = auth.rewriter({
+    users: 400,
+    items: 600,
+})
+
+server.use(rules);
+server.use(auth);
 server.use(router);
 
 server.listen(port);
